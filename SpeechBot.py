2709bf4a53dab4008223b8401aa7552614c037a6
Rich Peach <ivanuhka.99@mail.ru>
2023-02-07 03:31:28 +0300

    Devise choosing, error handling and over

--- SpeechBot.py
+++ SpeechBot.py
@@ -3,24 +3,50 @@ from pydub import AudioSegment
 import sounddevice as sd
 import soundfile as sf
 
-def say(text):
+def say(text: str, lang = 'ru'):
     # make a request to google to get synthesis 
-    txt = text
-    t1 = gtts.gTTS(text = txt ,lang = 'ru', slow = False)
+    t1 = gtts.gTTS(text = text ,lang = lang, slow = False)
     t1.save("tmp.mp3")
 
     sound = AudioSegment.from_mp3("tmp.mp3")
     sound.export("tmp.wav", format="wav")
 
-    #5 с наушниками
-    sd.default.device = 5
-
-    filename = 'tmp.wav'
     # Extract data and sampling rate from file
-    data, fs = sf.read(filename)  
+    data, fs = sf.read('tmp.wav')  
     sd.play(data, fs)
-    status = sd.wait()  # Wait until file is done playing
+    sd.wait()  #Wait until file is done playing
 
-while True:
-    text = input('Сказать:')
-    say(text)
+def specify_id():
+    #do while
+    while True:
+        device_id = input("Enter the device id:")
+        try:
+            sd.default.device = int(device_id)
+            break
+        except ValueError:
+            print("Your id is not an integer!")
+        except:
+            print("Somthing went wrong, try again")
+
+def main():
+    print("Choose your virtual output:")
+    print(sd.query_devices(),"\n")
+
+    specify_id()
+
+    print('''\nType "exit" to stop program\n''')
+    while True:
+        text = input('Say: ')
+        if text != 'exit':
+            try:
+                say(text)
+            except sd.PortAudioError:
+                print("\nYou've choosed the wrong device!")
+                specify_id()
+            except:
+                print("Somthing went wrong, try again")
+        else:
+            break
+
+if __name__ == "__main__":
+    main()
